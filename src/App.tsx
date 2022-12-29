090a3f268ec56ff77ad54043fe7a2ca828ec1090
Sean Gilbertson <sean.gilbertson@gmail.com>
2022-12-29 14:55:28 -0600

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,29 @@
 import { useAtom } from 'jotai';
-import ReactMarkdown from 'react-markdown';
 import './App.css';
 import logo from './logo.svg';
-import MarkdownLog from './models/MarkdownLog';
 import { windowsAtom, IWindow } from './stores';
 
-let log = new MarkdownLog();
-log.add('Hello there!');
 
-class NameHolder {
-  constructor(public name: string) { }
-}
 
-let nameHolder: NameHolder = new NameHolder('Cool name');
 
-// Functional component for viewing a MarkdownLog
-function MarkdownLogView(props: { log: MarkdownLog }) {
-  console.log(`> Rendering MarkdownLogView with ${props.log.entries.length} entries`);
-  return (
-    <ReactMarkdown key={props.log.entries.length}
-      children={props.log.joinAll()} />
-  );
-}
-
-function NameBlaster(props: { name: NameHolder }) {
-  console.log(`> Rendering NameBlaster with name ${props.name.name}`);
-  return (
-    <div>
-      <h1>{props.name.name}</h1>
-    </div>
-  );
-}
-
-console.log('hello from App.tsx');
-
-let stringName = 'A Cool Name';
-
-function StringNameDisplay(props: { name: string }) {
-  console.log(`> Rendering StringNameDisplay with name ${props.name}`);
-  return (
-    <div>
-      <h1>{props.name}</h1>
-    </div>
-  );
-}
-
-function updateStringName(newStringName: string) {
-  console.log(`> Updating string name to ${newStringName}`);
-  stringName = newStringName;
-}
 
 /**
  * Log all window IDs and the titles of their tabs. Log as JSON string.
  * @param windows Array of windows
  */
-// function logWindowsAndTabs(windows: chrome.windows.Window[]) {
 function logWindowsAndTabs(windows: IWindow[]) {
-  // console.log('> Windows and tabs:');
-  // windows.forEach((window, index) => {
-  //   console.log(`Window ${index}:`);
-  //   console.log(`Window ID: ${window.id}`);
-  //   console.log(`Window tab length: ${window.tabs && window.tabs.length}`);
-  // });
-
   const windowTabs: { tabs: { title: String }[] }[] = [];
   for (let window of windows) {
-    // console.log(`Window ID: ${window.id}`);
-    // console.log(`Window tab length: ${window.tabs && window.tabs.length}`);
     if (!window.tabs) {
       continue;
     }
     const tabTitles: String[] = window.tabs.map(tab => tab.title) as String[];
-    // windowTabs.push({title: tabTitles.join(', ')});
     windowTabs.push({ tabs: tabTitles.map(title => ({ title })) });
   }
   console.log(JSON.stringify(windowTabs));
-
-  // console.log('> Windows and tabs:');
-  // console.log(JSON.stringify(windows));
-
 }
 
 function App() {
-  // const windows = useWindowStore(state: => state.windows);
-  // const windows = useWindowStore((state: WindowState) => state.windows);
-  
   const [windows] = useAtom(windowsAtom);
   console.log('> windows:')
   console.log(windows);
@@ -101,20 +40,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <ReactMarkdown children={'Hello there'} /> */}
-        {/* <ReactMarkdown children={log.entries.join("\n")} /> */}
-        <MarkdownLogView log={log} />
-        <button onClick={() => log.add('neat')}>Add Log</button>
-        {/* >Check Tabs API</button> */}
-
-        <NameBlaster name={nameHolder} />
-        <button onClick={() => nameHolder.name = 'New name'}>Change Name</button>
-
-        <StringNameDisplay key={stringName} name={stringName} />
-        <button onClick={() => updateStringName('New string name')}>Change String Name</button>
-      </header>
-
       <main>
         <ul>
           {windowDebugInfo}
--- src/stores.ts
+++ src/stores.ts
@@ -21,78 +21,18 @@ class Tab implements ITab {
     }
 }
 
-// Implement the `isInGroup` function
-// Tab.prototype.isInGroup = function () {
-//     return this.groupId !== -1;
-// }
-
 export interface IWindow {
     id: number;
     tabs: ITab[];
 }
 
-/*
-import { createStore } from 'zustand';
-
-interface WindowState {
-    windows: chrome.windows.Window[];
-    loadWindows: () => void;
-}
-
-export const useWindowStore = createStore<WindowState>()((set) => ({
-    windows: [],
-    // loadWindows: () => set((state) => {
-    //     // set((state) => ({
-    //     //     windows: [
-    //     //         { id: 1, name: 'Window 1' },
-    //     //         { id: 2, name: 'Window 2' },
-    //     //         { id: 3, name: 'Window 3' },
-    //     //     ],
-    //     // }));
-
-    // })
-    loadWindows: () => {
-        // Load all windows and tabs from the Chrome API
-        chrome.windows.getAll({ populate: true }, (windows) => {
-            // set((state) => ({
-            //     windows: windows,
-            // }));
-            console.log('> windows:')
-            console.log(windows);
-            
-        });
-    }
-}));
-
-// export { useWindowStore };
-*/
 
 const fetchWindowsAtom = atom(async () => {
-    
-    /* 
-    return new Promise<chrome.windows.Window[]>((resolve, reject) => {
-        // If chrome API is not available, reject the promise
-        if (!chrome || !chrome.windows) {
-            reject(new Error('Chrome API is not available'));
-            return;
-        }
-
-        chrome.windows.getAll({ populate: true }, (windows) => {
-            resolve(windows);
-        });
-    });
-     */
 
     return new Promise<IWindow[]>((resolve, reject) => {
         const result: IWindow[] = [];
         // If chrome API is not available, return static data from `src/data/dev-tabs.json`
         if (!chrome || !chrome.windows) {
-            // reject(new Error('Chrome API is not available'));
-            // return;
-            // return fetch('./data/dev-tabs.json')
-            
-            // resolve(devTabs as IWindow[]);
-
             // For each object in devTabs, add to the result array
             devTabs.forEach((window) => {
                 const tabs: ITab[] = [];
@@ -105,12 +45,9 @@ const fetchWindowsAtom = atom(async () => {
         }
 
         chrome.windows.getAll({ populate: true }, (windows) => {
-            // resolve(windows);
             windows.forEach((window) => {
                 const tabs: ITab[] = [];
-                if (!window.tabs || !window.id) {
-                    return;
-                }
+                if (!window.tabs || !window.id) { return; }
                 window.tabs.forEach((tab) => {
                     if (!tab || tab.id === undefined || tab.title === undefined || tab.groupId === undefined) {
                         return;
@@ -123,7 +60,7 @@ const fetchWindowsAtom = atom(async () => {
         resolve(result);
     });
 });
-// const windowsAtom = atom<chrome.windows.Window[]>([]);
+
 const windowsAtom = atom(async (get) => {
     return get(fetchWindowsAtom);
 });
