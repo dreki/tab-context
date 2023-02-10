0416b75f6be879099872a7b9eda8363e15c9aec0
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-02-09 21:55:03 -0600

    Lay out session tabs

--- src/components/FavIconWithDefault.tsx
+++ src/components/FavIconWithDefault.tsx
@@ -1,10 +1,25 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
 import { ITab } from "../types/ITab";
 
-export function FavIconWithDefault({ tab }: { tab: ITab }) {
-  let favIconSrc: string | undefined = tab.favIconUrl;
-  if (!favIconSrc) {
-    return <GlobeAltIcon className="h-4 w-4" />;
-  }
-  return <img src={favIconSrc} alt="tab icon" className="h-4 w-4" />;
+interface IFavIconWithDefaultProps {
+    tab: ITab;
+    // Additional className
+    className?: string;
+}
+
+export function FavIconWithDefault({
+    tab,
+    className = "",
+}: IFavIconWithDefaultProps) {
+    let favIconSrc: string | undefined = tab.favIconUrl;
+    if (!favIconSrc) {
+        return <GlobeAltIcon className={`inline-block h-4 w-4 ${className}`} />;
+    }
+    return (
+        <img
+            src={favIconSrc}
+            alt="tab icon"
+            className={`h-4 w-4 ${className} inline-block`}
+        />
+    );
 }
--- src/components/TabDetailComponent.tsx
+++ src/components/TabDetailComponent.tsx
@@ -1,4 +1,5 @@
 import { ITab } from "../types/ITab";
+import { FavIconWithDefault } from "./FavIconWithDefault";
 
 interface TabDetailComponentProps {
     tab: ITab;
@@ -11,8 +12,20 @@ export function TabDetailComponent({ tab }: TabDetailComponentProps) {
         //         <div className="text-sm text-gray-700">{tab.title}</div>
         //     </div>
         // </div>
-        <div>
-            {tab.title}
+
+        // <div className="rounded-md border border-indigo-400 p-1">
+        //     <FavIconWithDefault tab={tab} />
+        //     {tab.title}
+        // </div>
+
+        // Lay out a FavIconWithDefault to the left of tab.title.
+        <div className="flex flex-row">
+            <div>
+                <FavIconWithDefault tab={tab} className={"align-middle"} />
+            </div>
+            <div>
+                <div className="h-5 pl-1 align-middle">{tab.title}</div>
+            </div>
         </div>
     );
 }
