7a92058790ff9a5b03a2c215930b3fac15a4eda1
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-02-25 21:46:13 -0600

    Handle more situations

--- src/utils/onOurTabActivated.tsx
+++ src/utils/onOurTabActivated.tsx
@@ -1,4 +1,3 @@
-
 interface IOnOurTabActivatedParams {
     // An async function to call when our tab is activated.
     callback: () => Promise<void>;
@@ -8,12 +7,36 @@ export function onOurTabActivated({
     callback: onOurTabActivated,
 }: IOnOurTabActivatedParams) {
     // Listen for our tab to become activated
-    chrome.tabs.onActivated.addListener((activeInfo) => {
-        chrome.tabs.getCurrent((tab) => {
-            if (!tab) {
+    chrome.tabs.onActivated.addListener(
+        (activeInfo: chrome.tabs.TabActiveInfo): void => {
+            chrome.tabs.getCurrent((tab) => {
+                if (!tab) {
+                    return;
+                }
+                if (tab.id === activeInfo.tabId) {
+                    onOurTabActivated();
+                }
+            });
+        }
+    );
+
+    chrome.tabs.onCreated.addListener((tab) => {
+        chrome.tabs.getCurrent((currentTab) => {
+            if (!currentTab) {
                 return;
             }
-            if (tab.id === activeInfo.tabId) {
+            if (currentTab.id === tab.id) {
+                onOurTabActivated();
+            }
+        });
+    });
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        chrome.tabs.getCurrent((currentTab) => {
+            if (!currentTab) {
+                return;
+            }
+            if (currentTab.id === tabId) {
                 onOurTabActivated();
             }
         });
