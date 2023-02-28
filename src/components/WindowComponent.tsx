41e79fd02fe651137f7fac0c48eaa11bbe82703c
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-02-27 21:50:46 -0600

    Show closed tab count

--- src/components/WindowComponent.tsx
+++ src/components/WindowComponent.tsx
@@ -23,24 +23,36 @@ interface WindowComponentProps {
 export const WindowComponent = observer(function WindowComponent(
     props: WindowComponentProps
 ) {
-    const closedTabs =
+    /*
+    let closedTabs =
         props.closedTabs?.tabs?.map((tab) => {
             return <li key={tab.id}>{tab.title}</li>;
         }) || null;
+    */
+    
+    // Show "X Closed Tabs" if there are closed tabs, even if there are 0.
+    let closedTabs = <>...</>;
+    if (props.closedTabs) {
+        closedTabs = (
+            <p className="font-medium text-base">
+                {props.closedTabs.tabs.length} Closed Tabs
+            </p>
+        );
+    }
     return (
         <div className="card-bordered card card-compact mb-8 bg-slate-50 shadow-md">
             {/* Hidden h2, for accessibility, noting window */}
             <h2 className="sr-only">Window {props.window.index}</h2>
             <div className="overflow-hidden rounded-t-2xl bg-slate-200 p-2">
+                {/* Hidden h3 to note tab list */}
+                <h3 className="sr-only">Tab List</h3>
                 <TabList tabs={props.window.tabs} />
             </div>
             <div className="card-body">
                 <div>
-                    Window ID: {props.window.id} (index: {props.window.index})
-                </div>
-                <div>
-                    <h3>Closed Tabs</h3>
-                    <ul>{closedTabs}</ul>
+                    {/* h3 SR only to note closed tabs label */}
+                    <h3 className="sr-only">Closed Tabs</h3>
+                    {closedTabs}
                 </div>
                 <div className="card-actions mt-2">
                     <button
