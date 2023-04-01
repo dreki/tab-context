f87c1bad6d20c594a6fd6e2508db2ae61e813c1a
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-03-31 22:37:11 -0500

    Init showing tab actions for rollovert

--- src/components/TabDetailComponent.tsx
+++ src/components/TabDetailComponent.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ITab } from "../types/ITab";
 import { FavIconWithDefault } from "./FavIconWithDefault";
 
@@ -7,6 +8,9 @@ interface TabDetailComponentProps {
 }
 
 export function TabDetailComponent({ tab, key }: TabDetailComponentProps) {
+    // useState for whether to show tab actions
+    const [showTabActions, setShowTabActions] = useState(false);
+
     return (
         <li className="flex items-center space-x-3" key={key}>
             <FavIconWithDefault tab={tab} className={"h-5 w-5 flex-shrink-0"} />
